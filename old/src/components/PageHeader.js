d48a00d2d20e69565a4d44c0b736aed640a141ff
pau1o-hs <henrique_phs117@usp.br>
2020-04-21 17:18:56 -0300

    Single-File Component update

---
+++ old/src/components/PageHeader.js
@@ -0,0 +1,10 @@
+Vue.component('ferashop', 
+{
+	template: `
+		<div class="container">
+			<p><h1><strong>Fera Shopping</strong></h1><p>
+			<hr />
+			<br><br>
+		</div>
+	`
+})
---
+++ vue-cli/src/components/PageHeader.vue
@@ -0,0 +1,3 @@
+<template>
+	<h1>PETSHOP</h1>
+</template>
